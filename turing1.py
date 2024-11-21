22d1abe4b81113d7fc718076f9473ca95deae13e
Fikiremariyam <dgi123baba@gmail.com>
2024-11-21 15:36:30 +0200

    turing prepatation

--- turing1.py
+++ turing1.py
@@ -1,32 +1,61 @@
-x , s , y = [1,6,3] , [2,2,5] , [1 ,2 ,3, 4,5,6,7,8,9]
+""" 
+frogs question
+*** approach ***
 
-y.sort()
-def findLeft(val):
-    l , r = 0 , len(y) - 1
-    while l <= r:
-        mid = (r - l) // 2 + l 
+1 fined alll the input 
 
-        if y[mid] < val:
-            l = mid + 1
-        elif y[mid] >= val:
-            r = mid - 1
+2. sort the filies list and intitate  the  result list 
+3 .iterate over each forgs 
+4 .on each iteration fined the  flies that are inside the radious of  the frog tounge 
+    i.e postion - tounge size   << ---->>  postion + tounge size
+     fined these by using biniary search 
+5 ,append: the right boundry - the left boundry +1 to the result array 
+
+time complexity  :    O( n(log n) )
+
+space complextity : O ( n )
+
+
+"""
+def findleft(target,y):
     
-    return l
+    left=0
+    right=len(y)-1
 
-def findRight(val):
-    l , r = 0 , len(y) - 1
+    while left <= right:
+        mid=(right - left)//2 + left
 
-    while l <= r:
-        mid = (r - l) // 2 + l 
-        
-        if y[mid] <= val:
-            l = mid + 1
-        elif y[mid] > val:
-            r = mid - 1
-    return r
+        if y[mid] <target :
+            left=mid+1
+        elif y[mid] >= target :
 
-res = []
-for ind,val in enumerate(x):
-    right , left = findRight(val+s[ind]), findLeft(val-s[ind])
-    res.append(right - left + 1)
-print(res)
+            right =mid -1
+    return left
+
+def findright(target,y) :
+    left=0
+    right=len(y)-1
+
+    while left <= right:
+        mid= (right - left) //2 +left
+        if y[mid] > target :
+            right-=1
+        elif y[mid] <=target:
+            left+=1
+
+    return right
+
+
+
+
+def solution(x,s,y):
+    res=[]
+    y.sort()
+    for i in range(len(s)):
+        res.append( (findright(x[i]+s[i],y) - findleft(x[i]-s[i] ,y) ) + 1 )
+    return res
+
+
+
+x , s , y = [1,6,3] , [2,2,5] , [1 ,2 ,3, 4,5,6,7,8,9]
+print(solution(x,s,y))
