878935107c28a09f67179a86314cd3ae506630ee
Fikiremariyam <dagi123baba@gmail.com>
2025-02-01 15:39:18 +0300

    code force 998

---
+++ A_Fibonacciness.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    array=list(map(int,input().split()))
+    final=0
+    for i in range(-500,500):
+        curr=0
+        curr += 1 if array[0]+array[1] ==i else 0
+        curr +=1  if array[1]+i == array[2] else 0 
+        curr+= 1 if  array[2]+i ==  array[3] else 0
+        final=max(final,curr)
+    print(final)
---
+++ B_Farmer_John_s_Card_Game.py
@@ -0,0 +1,19 @@
+
+for _ in range(int(input())):
+    n,m=map(int,input().split())
+    final=[]
+    for i in range(n):
+        curr=list(map(int,input().split()))
+        curr.sort()
+        final.append(final)
+        final
+    c=0
+    for i in range(m):
+        for j in range(n):
+            if final[i][j] != c :
+                print(-1)
+                break
+        
+    c+=1
+
+        print(final)
